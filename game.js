9f6c6c10705f6e9796ac5c8c467d28aec6216ff1
Piotr Petryka <ppwxp19@gmail.com>
2022-10-18 14:03:04 +0200

    player movemant works (without collistion)

--- game.js
+++ game.js
@@ -92,25 +92,45 @@ function create_map(map) {
         }
         divh += "<div>" + divw + "</div>"
     }
-    divh += "<div id=\"Character\">\n" +
-        "   <img class=\"Character_shadow pixelart\" src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png\" alt=\"Shadow\" />\n" +
-        "   <img class=\"Character_spritesheet pixelart face-down\" src=\"sheet/_Idle.png\" alt=\"Character\" />\n" +
-        "</div>"
-    console.log(divh)
     document.getElementById('container').innerHTML = divh
 }
-
+function charMove() {
+    document.getElementById("Character-moves").src="sheet/_Run.png";
+}
+function charStop() {
+    document.getElementById("Character-moves").src = "sheet/_Idle.png";
+}
 function up() {
     document.getElementById("Character").style.top = parseInt(document.getElementById("Character").style.top ) - 10 + "px"
+    charMove();
+}
+function upStop() {
+    document.getElementById("Character").style.top = parseInt(document.getElementById("Character").style.top ) - 10 + "px"
+    charStop();
 }
 function down() {
     document.getElementById("Character").style.top = parseInt(document.getElementById("Character").style.top ) + 10 + "px"
+    charMove();
+}
+function downStop() {
+    document.getElementById("Character").style.top = parseInt(document.getElementById("Character").style.top ) + 10 + "px"
+    charStop()
 }
 function left() {
     document.getElementById("Character").style.left = parseInt(document.getElementById("Character").style.left ) - 10 + "px"
+    charMove();
+}
+function leftStop() {
+    document.getElementById("Character").style.left = parseInt(document.getElementById("Character").style.left ) - 10 + "px"
+    charStop()
 }
 function right() {
     document.getElementById("Character").style.left = parseInt(document.getElementById("Character").style.left ) + 10 + "px"
+    charMove();
+}
+function rightStop() {
+    document.getElementById("Character").style.left = parseInt(document.getElementById("Character").style.left ) + 10 + "px"
+    charStop()
 }
 
 initGame();
@@ -121,12 +141,13 @@ function initGame() {
     document.getElementById("Character").style.top = "-1150px"
     document.onkeydown = (e) => {
       e = e || window.event;
-      if (e.keyCode === 38) up()
-      else if (e.keyCode === 37) left()
-      else if (e.keyCode === 39) right()
-      else if (e.keyCode === 40) down()
+      if (e.keyCode === 38) setTimeout(() => {upStop()}, 250, up())
+      else if (e.keyCode === 37) setTimeout(() => {leftStop()}, 250, left())
+      else if (e.keyCode === 39) setTimeout(() => {rightStop()}, 250, right())
+      else if (e.keyCode === 40) setTimeout(() => {downStop()}, 250, down())
 
 }
+    console.log(document.getElementById("Character_spritesheet pixelart face-down"))
 
     // Your game can start here, but define separate functions, don't write everything in here :)
 
