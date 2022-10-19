b53d2297507ecb9ebd82bdd3be7cfc9282f6009f
MonikaMarkulis <markulismonika@gmail.com>
2022-10-19 09:37:27 +0200

    update

--- game.js
+++ game.js
@@ -4,9 +4,10 @@ var animation_time = 25
 initGame();
 
 function initGame() {
-    element_map(level_2_mapD);
+    element_map(test);
     console.log(maps)
-
+    document.getElementById("Character").style.left = "-500px"
+    document.getElementById("Character").style.top = "-1200px"
     click_move();
 
     // Your game can start here, but define separate functions, don't write everything in here :)
